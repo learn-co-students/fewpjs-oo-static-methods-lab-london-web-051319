0ab9cb38b3b0e68348af7b49ff1ff7802a5cfaa1
mbarrins <maija.barrins@gmail.com>
2019-07-09 20:08:43 +0100

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.slice(0,1).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^a-z0-9-'\s]/gi,'');
+  }
+
+  static titleize(string) {
+    const excluded_words = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by','from']
+    const new_arr = []
+    string.split(" ").forEach((word, index) => {
+      if (excluded_words.includes(word) && index !== 0) {
+        new_arr.push(word)
+      } else {
+        new_arr.push(this.capitalize(word))
+      }
+    })
+    return new_arr.join(" ")
+  }
 }
