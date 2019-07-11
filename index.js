4a51c7f97eb5f071b31e8b3ed0df8051bf04b4cc
flexkat <catherinemaritz1@gmail.com>
2019-07-11 09:09:37 +0100

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9- ']+/g, '');
+  }
+
+  static titleize(string) {
+    let badWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let words = string.split(" ");
+    const titlized = words.map(word => {
+      if (!badWords.includes(word)) {
+        word = word.charAt(0).toUpperCase() + word.slice(1)
+        return word;
+      }
+      else if (words[0] == word) {
+        word = word.charAt(0).toUpperCase() + word.slice(1)
+        return word;
+      }
+      else {
+        return word;
+      }
+    })
+    return titlized.join(" ")
+  }
 }
