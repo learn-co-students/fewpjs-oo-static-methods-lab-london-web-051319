3c4766aa13dfdcfaac21e65ae68215b5960508d2
moniet <moniet.sawhney@gmail.com>
2019-07-13 21:04:07 +0100

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9 '-]/g, '');
+  }
+
+  static titleize(str) {
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const newStr = [];
+    const strSplit = str.split(" ");
+
+    for (let w of strSplit ) {
+      if (exceptions.includes(w)) {
+        newStr.push(w);
+      } else {
+        newStr.push(this.capitalize(w));
+      }
+    }
+
+
+    return this.capitalize(newStr.join(" "));
+  }
 }
